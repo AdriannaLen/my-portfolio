8eb5bd9099bd70a8bf06a5060e27053ab76b3aba
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-07-29 09:54:44 +0200

    cleared and formatted code

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+      id: "aboutme",
+      title: "About Me",
+    },
+    {
+      id: "projects",
+      title: "Projects",
+    },
+    {
+      id: "carieer",
+      title: "Carieer",
+    },
+    {
+        id: "technologies",
+        title: "Technologies",
+      },
+    {
+      id: "contact",
+      title: "Contacts",
+    },
+  ];
