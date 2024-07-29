bcbf49ce27a277dccebf801c07b1e3549f8e6f9d
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-07-29 11:23:28 +0200

    added aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import Navbar from './components/Navbar'
+import AboutMe from './components/AboutMe.jsx'
+import styles from './style.js'
+
 const App = () => {
   return (
-    <div>
-      
+    <>
+    <div className="bg-slate-300 w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
+    </div>
+     <AboutMe />
+     </>
   )
 }
 
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,11 @@
+
+const AboutMe = () => {
+  return (
+    <div className="font-poppins flex-col w-full flex justify-center py-6 bg-dimWhite h-[100] text-[24px]">
+        <h1 className="text-center mt-10">Adrianna Lenczewska</h1>
+      <h2 className="text-center">Front End Developer</h2>
+    </div>
+  )
+}
+
+export default AboutMe
