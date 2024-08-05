333dc8f307a37eca3a1a3b32691ba2d32cfc15b4
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-08-05 11:25:46 +0200

    added horizontal line to me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,7 @@
 import { technologies } from "../constants";
 const AboutMe = () => {
   return (
-    <div className="h-[100vh] font-poppins flex-col w-full flex justify-center py-12 bg-slate-800">
+    <div className="h-[100vh] font-poppins flex-col w-full flex justify-center bg-slate-800">
        <ul className="flex flex-wrap justify-center transition-transform duration-500 hover:scale-105 animate-fadeIn">
       {technologies.map((tech) => (
         <li key={tech.id} className="m-8">
@@ -10,11 +10,11 @@ const AboutMe = () => {
       ))}
     </ul>
       <div className="flex justify-center items-center space-x-4">
-    <h1 className="text-[24px] sm:text-[32px] uppercase tracking-wide text-dimWhite transform transition-transform duration-500 hover:scale-105">
+    <h1 className="text-[24px] sm:text-[32px] uppercase tracking-wide text-dimWhite">
       Adrianna Lenczewska
     </h1>
     <div className="h-[24px] sm:h-[32px] border-r-4 border-dimWhite"></div>
-    <h2 className="text-[20px] sm:text-[32px] uppercase tracking-wide text-dimWhite transform transition-transform duration-500 hover:scale-105">
+    <h2 className="text-[20px] sm:text-[32px] uppercase tracking-wide text-dimWhite">
       Front End Developer
     </h2>
   </div>
--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,17 +1,18 @@
 
 const Me = () => {
   return (
-      <div id="me" className="h-[100vh] flex flex-col items-center lg:flex-row lg:justify-around">
-        <p className="font-poppins mt-20 text-center w-[45%] text-black text-lg sm:text-xl leading-relaxed space-y-4 p-6 rounded-lg transition-transform duration-500 hover:scale-105 animate-fadeIn">
+    <div id="me" className="h-[100vh] flex items-center lg:flex-row lg:justify-start">
+    <div className="h-full w-0.5 bg-black ml-32"></div>
+    <p className="font-poppins ml-4 mt-20 text-center w-[45%] text-black text-lg sm:text-xl leading-relaxed space-y-4 p-6 rounded-lg transition-transform duration-500 hover:scale-105 animate-fadeIn">
         I am an experienced subcontracting buyer with 7 years of experience within international organization. This role
- honed my negotiation skills, relationship management, and precise planning abilities as well as attention to
- details. It helped understand IT environment and get to know the role of developers as I’ve been contracting
- them in terms of their roles. In pursuit of a career change, I began studying front-end development through
- Udemy. I acquire knowledge from online courses, video tutorials, and my studies at Software Academy. I began
- building small projects as a freelance. I am highly motivated, a quick learner, and excel at problem-solving. My
- superpower is combining attention to detail with creativity, which is invaluable in front-end development.
-        </p>
-      </div>
+        honed my negotiation skills, relationship management, and precise planning abilities as well as attention to
+        details. It helped understand IT environment and get to know the role of developers as I’ve been contracting
+        them in terms of their roles. In pursuit of a career change, I began studying front-end development through
+        Udemy. I acquire knowledge from online courses, video tutorials, and my studies at Software Academy. I began
+        building small projects as a freelance. I am highly motivated, a quick learner, and excel at problem-solving. My
+        superpower is combining attention to detail with creativity, which is invaluable in front-end development.
+    </p>
+</div>
   )
 }
 
