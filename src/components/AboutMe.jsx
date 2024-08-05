805998fa2f26234d30c624c6b9c500a7c285e4d0
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-08-05 12:35:37 +0200

    latest commit

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,14 +1,7 @@
 import { technologies } from "../constants";
 const AboutMe = () => {
   return (
-    <div className="h-[100vh] font-poppins flex-col w-full flex justify-center bg-slate-800">
-       <ul className="flex flex-wrap justify-center transition-transform duration-500 hover:scale-105 animate-fadeIn">
-      {technologies.map((tech) => (
-        <li key={tech.id} className="m-8">
-          <img src={tech.img} alt={tech.name} className="w-16 h-16" />
-        </li>
-      ))}
-    </ul>
+    <div className="h-[80vh] font-poppins flex-col w-full flex justify-center bg-slate-800">
       <div className="flex justify-center items-center space-x-4">
     <h1 className="text-[24px] sm:text-[32px] uppercase tracking-wide text-dimWhite">
       Adrianna Lenczewska
@@ -18,6 +11,13 @@ const AboutMe = () => {
       Front End Developer
     </h2>
   </div>
+  <ul className="flex flex-wrap justify-center transition-transform duration-500 hover:scale-105 animate-fadeIn">
+      {technologies.map((tech) => (
+        <li key={tech.id} className="m-8">
+          <img src={tech.img} alt={tech.name} className="w-16 h-16" />
+        </li>
+      ))}
+    </ul>
     </div>
   );
 };
