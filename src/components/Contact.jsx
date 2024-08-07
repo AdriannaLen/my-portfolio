f4310ecb57a5e5ac489c68a7904a353ee6015cbb
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-08-07 08:37:03 +0200

    changed email and -mt in contact for form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,14 @@
+
+import github from '../assets/github.svg'
+import linkedin from '../assets/linkedin.svg'
+
 const Contact = () => {
   return (
-    <div id="contact" className="bg-white min-h-screen sm:h-[80vh] flex justify-center items-center relative py-16">
+    <div id="contact" className="bg-white min-h-screen sm:min-h-[70vh] flex justify-center items-center relative py-16">
       <div className="flex flex-col sm:flex-row justify-between items-start w-full max-w-6xl p-4 sm:p-8">
         {/* Formularz kontaktowy */}
-        <div className="w-full sm:w-1/2 max-w-lg flex-shrink-0 relative z-10">
-          <form className="bg-white p-8 border w-full h-auto relative sm:-top-16">
-            <h1 className="text-2xl font-bold text-center">Kontakt</h1>
+        <div className="w-full sm:w-1/2 max-w-lg flex-shrink-0 relative z-10 -mt-24 sm:-mt-48">
+          <form className="bg-white p-8 border w-full h-auto relative">
             <div className="flex flex-col mt-6">
               <label className="mb-2 text-sm font-medium">Imię</label>
               <input type="text" className="p-3 border rounded-lg" required />
@@ -29,20 +32,24 @@ const Contact = () => {
         </div>
 
         {/* Dane kontaktowe */}
-        <div className="w-full sm:w-1/2 max-w-lg flex-shrink-0 text-center">
+        <div className="w-full sm:w-1/2 max-w-lg flex-shrink-0 text-center mt-8 sm:mt-0">
           <div className="p-8 font-poppins bg-white border h-full">
-            <p className="mb-2 text-[32px]"><strong>Kontakt</strong></p>
-            <p className="mb-2"><strong>Adrianna Lenczewska</strong></p>
-            <p className="mb-2"><strong>ul.Gdańska, Więcbork 89-410</strong></p>
-            <p className="mb-2"><strong>+48 669 961 266</strong></p>
-            <p className="mb-2"><strong>info@example.com</strong></p>
+            <p className="mb-2 text-[32px]">Kontakt</p>
+            <p className="mb-2">Adrianna Lenczewska</p>
+            <p className="mb-2">ul.Gdańska, Więcbork 89-410</p>
+            <p className="mb-2">+48 669 961 266</p>
+            <p className="mb-2">adriannalenn@gmail.com</p>
             <p>Jeżeli masz jakieś pytanie, możesz skontaktować się ze mną za pomocą formularza!</p>
+            <span className="flex flex-row justify-center py-6">
+            <a href="https://github.com/AdriannaLen" className="hide-on-mobile"><img src={github} alt="github" className="object-contain" /></a>
+            <a href="https://www.linkedin.com/in/adrianna-lenczewska-276185287/" className="hide-on-mobile"><img src={linkedin} alt="linkedin" className="object-contain" /></a>
+            </span>
           </div>
         </div>
+        <div className="hidden sm:block absolute h-full w-0.5 bg-slate-600 mr-32 lg:right-0 top-0"></div>
       </div>
     </div>
   );
 };
-
 
 export default Contact;
