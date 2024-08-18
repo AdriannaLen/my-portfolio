b4c3d5fe776ee3fbe59ce0212f3d464b6a8ae15f
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-08-18 13:45:39 +0200

    changed places form and contact  details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,22 @@ const Contact = () => {
   return (
     <div id="contact" className="bg-white min-h-screen sm:min-h-[70vh] flex justify-center items-center relative py-16">
   <div className="flex flex-col sm:flex-row justify-between items-start w-full max-w-6xl p-4 sm:p-8">
+    {/* Dane kontaktowe */}
+    <div className="w-full sm:w-1/2 flex-shrink-0 text-start mt-8 sm:mt-0">
+      <div className="p-8 font-poppins bg-white h-full">
+        <p className="mb-2 text-[46px]">Kontakt</p>
+        <p className="mb-2">Adrianna Lenczewska</p>
+        <p className="mb-2">ul.Gdańska, Więcbork 89-410</p>
+        <p className="mb-2">+48 669 961 266</p>
+        <p className="mb-2">adriannalenn@gmail.com</p>
+        <p>Jeżeli masz jakieś pytanie, możesz skontaktować się ze mną za pomocą formularza!</p>
+        <span className="flex flex-row justify-center py-6">
+        <a href="https://github.com/AdriannaLen" className="hide-on-mobile"><img src={github} alt="github" className="object-contain w-[44px]" /></a>
+        <a href="https://www.linkedin.com/in/adrianna-lenczewska-276185287/" className="hide-on-mobile"><img src={linkedin} alt="linkedin" className="object-contain" /></a>
+        </span>
+      </div>
+    </div>
+    <div className="hidden sm:block absolute h-full w-0.5 bg-slate-600 ml-56 lg:left-0 top-0"></div>
     {/* Formularz kontaktowy */}
     <div className="w-full sm:w-1/2 flex-shrink-0 relative z-10 -mt-24 sm:-mt-48 sm-mr-8">
       <form className="bg-white p-8 border w-full h-auto relative">
@@ -30,23 +46,6 @@ const Contact = () => {
         </button>
       </form>
     </div>
-
-    {/* Dane kontaktowe */}
-    <div className="w-full sm:w-1/2 flex-shrink-0 text-center mt-8 sm:mt-0 sm:ml-8">
-      <div className="p-8 font-poppins bg-white border h-full">
-        <p className="mb-2 text-[32px]">Kontakt</p>
-        <p className="mb-2">Adrianna Lenczewska</p>
-        <p className="mb-2">ul.Gdańska, Więcbork 89-410</p>
-        <p className="mb-2">+48 669 961 266</p>
-        <p className="mb-2">adriannalenn@gmail.com</p>
-        <p>Jeżeli masz jakieś pytanie, możesz skontaktować się ze mną za pomocą formularza!</p>
-        <span className="flex flex-row justify-center py-6">
-        <a href="https://github.com/AdriannaLen" className="hide-on-mobile"><img src={github} alt="github" className="object-contain" /></a>
-        <a href="https://www.linkedin.com/in/adrianna-lenczewska-276185287/" className="hide-on-mobile"><img src={linkedin} alt="linkedin" className="object-contain" /></a>
-        </span>
-      </div>
-    </div>
-    <div className="hidden sm:block absolute h-full w-0.5 bg-slate-600 mr-32 lg:right-0 top-0"></div>
   </div>
 </div>
   );
