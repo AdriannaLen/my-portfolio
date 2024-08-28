40c235e34a307d6a9c29c2a5bd18082829c7aeae
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-08-28 11:14:49 +0200

    working form + succes button

---
+++ src/components/Alert.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+const Alert = ({ message, onClose }) => {
+  if (!message) return null;
+
+  return (
+    <div
+      className="fixed top-12 align-middle p-4 mb-4 text-sm rounded-lg"
+      style={{
+        backgroundColor: "#ede9e3",
+        color: "#4B3621",
+      }}
+      role="alert"
+    >
+      <span className="font-medium">Success alert!</span> {message}
+      <button
+        className="ml-4 font-bold"
+        style={{
+          color: "#4B3621",
+        }}
+        onClick={onClose}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+
+Alert.propTypes = {
+  message: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Alert;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,37 @@
 import { useState, useEffect } from "react";
 import github from "../assets/github.svg";
 import linkedin from "../assets/linkedin.svg";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const [isContactVisible, setIsContactVisible] = useState(false);
   const [isFormVisible, setIsFormVisible] = useState(false);
+  const [alertMessage, setAlertMessage] = useState(null);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b3e5a83b-3af3-4fe3-a185-1751dfb0e416");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      setAlertMessage("Your message was sent successfully!");
+    } else {
+      setAlertMessage("There was an error sending your message.");
+    }
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -14,7 +41,6 @@ const Contact = () => {
         .querySelector("form")
         .getBoundingClientRect();
 
-      // Animacja w momencie przewinięcia na sekcję "Contact"
       if (
         contactRect.top < window.innerHeight * 0.75 &&
         contactRect.bottom > 0
@@ -24,7 +50,6 @@ const Contact = () => {
         setIsContactVisible(false);
       }
 
-      // Animacja w momencie przewinięcia na formularz
       if (formRect.top < window.innerHeight * 0.75 && formRect.bottom > 0) {
         setIsFormVisible(true);
       } else {
@@ -39,13 +64,16 @@ const Contact = () => {
     };
   }, []);
 
+  const handleCloseAlert = () => {
+    setAlertMessage(null);
+  };
+
   return (
     <div
       id="contact"
       className="bg-white min-h-screen sm:min-h-[70vh] flex justify-center items-center relative py-16 overflow-x-hidden"
     >
       <div className="flex flex-col sm:flex-row justify-between items-start w-full max-w-6xl p-4 sm:p-8">
-        {/* Dane kontaktowe */}
         <div
           className={`w-full sm:w-1/2 flex-shrink-0 text-start mt-8 sm:mt-0 relative transition-transform duration-700 ease-out ${
             isContactVisible ? "translate-x-0 opacity-100 visible" : "-translate-x-full opacity-0 invisible"
@@ -82,7 +110,11 @@ const Contact = () => {
                 href="https://www.linkedin.com/in/adrianna-lenczewska-276185287/"
                 className="hide-on-mobile"
               >
-                <img src={linkedin} alt="linkedin" className="object-contain" />
+                <img
+                  src={linkedin}
+                  alt="linkedin"
+                  className="object-contain"
+                />
               </a>
             </span>
           </div>
@@ -90,37 +122,28 @@ const Contact = () => {
 
         <div className="hidden sm:block absolute h-full w-0.5 bg-slate-600 left-[15%] sm:left-[15%] lg:left-[15%] top-0"></div>
 
-        {/* Formularz kontaktowy */}
         <div
           className={`w-full sm:w-1/2 mt-[20vh] sm:mt-0 flex-shrink-0 relative z-10 transition-transform duration-700 ease-out ${
             isFormVisible ? "translate-x-0 opacity-100 visible" : "translate-x-full opacity-0 invisible"
           } sm:mr-8 lg:mr-[25%]`}
         >
-          <form className="bg-lightPeach p-8 border w-full h-auto relative">
+          <form onSubmit={onSubmit} className="bg-lightPeach p-8 border w-full h-auto relative">
             <div className="flex flex-col mt-6">
               <label className="mb-2 text-sm font-medium">Name</label>
-              <input type="text" className="p-3 border rounded-lg" required />
+              <input type="text" name="name" className="p-3 border rounded-lg" required />
             </div>
             <div className="flex flex-col mt-4">
               <label className="mb-2 text-sm font-medium">Email</label>
-              <input type="email" className="p-3 border rounded-lg" required />
+              <input type="email" name="email" className="p-3 border rounded-lg" required />
             </div>
             <div className="flex flex-col mt-4">
               <label className="mb-2 text-sm font-medium">Message</label>
               <textarea
                 className="p-3 border rounded-lg"
+                name="message"
                 rows="4"
                 required
               ></textarea>
-            </div>
-            <div className="flex items-center mt-4">
-              <input type="checkbox" className="mr-2" required />
-              {/* <label className="text-sm">
-                Zgadzam się na{" "}
-                <a href="#" className="text-text">
-                  politykę prywatności
-                </a>
-              </label> */}
             </div>
             <button
               type="submit"
@@ -131,6 +154,10 @@ const Contact = () => {
           </form>
         </div>
       </div>
+
+      {alertMessage && (
+        <Alert message={alertMessage} onClose={handleCloseAlert} />
+      )}
     </div>
   );
 };
